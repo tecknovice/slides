d3f023fd6689e9cd7962480f4624c68233b6fc70
Nguyen Van Hung <tecknovice@gmail.com>
2025-06-12 16:13:56 +0900

    Update repository name from introduction to slides

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/introduction">
+    <BrowserRouter basename="/slides">
       <App />
     </BrowserRouter>
   </StrictMode>,
